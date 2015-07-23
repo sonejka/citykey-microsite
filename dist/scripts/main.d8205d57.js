8abd20836abfb9e326a7dcc448051a4f86b71e2d
Kseniya Varabei <kseniya.varabei@gmail.com>
2015-07-23 18:25:41 +0300

    update content
    - instagram feed
    - center block

---
+++ dist/scripts/main.d8205d57.js
@@ -0,0 +1,1 @@
+window.onload=function(){$("#instagram-feed").pongstgrm({accessId:"2121083481",accessToken:"2121083481.1677ed0.8737974840cf46309a44befee7cfdd38",show:"feed",count:6,column:"col-xs-12 col-sm-4 col-md-4 col-lg-4",button:"btn btn-success",comments:!1,effects:"scaling"})};
