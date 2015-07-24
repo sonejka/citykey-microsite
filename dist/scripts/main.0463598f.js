d9d6a4e8482072fec6fd748a13b6ffbd988dc497
Kseniya Varabei <kseniya.varabei@gmail.com>
2015-07-24 18:15:33 +0300

    update homepage

---
+++ dist/scripts/main.0463598f.js
@@ -0,0 +1,1 @@
+window.onload=function(){$("#instagram-feed").pongstgrm({accessId:"2121083481",accessToken:"2121083481.1677ed0.8737974840cf46309a44befee7cfdd38",show:"feed",count:6,column:"col-xs-12 col-sm-4 col-md-4 col-lg-4",button:"btn btn-default",comments:!1,effects:"scaling"})};
