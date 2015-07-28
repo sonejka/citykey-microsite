f402471dec9fdc5f9d56faf3a5875efa75518126
Kseniya Varabei <kseniya.varabei@gmail.com>
2015-07-28 17:06:59 +0300

    add activity page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,10 @@
 // jshint devel:true
 window.onload = function() {
 
-//  $("#welcome-modal").modal();
+  //$("#welcome-modal").modal();
 
     $( ".sidebar-nav-wrap" ).hover(function() {
             $(".content-with-sidebar").toggleClass("sidebar-opened");
-//        $( ".shadow-box" ).animate({
-//            opacity: 1
-//        }, 1500 );
     });
 
     $( ".shadow-box" ).hover(function() {
@@ -18,7 +15,7 @@ window.onload = function() {
   $('#instagram-feed').pongstgrm({
     accessId:     '2121083481',
     accessToken:  '2121083481.1677ed0.8737974840cf46309a44befee7cfdd38',
-    show:         'feed',     //show: 'themostbeautifulplacesonearth'
+    show:         'feed',
     count:         6,
     column:        "col-xs-12 col-sm-4 col-md-4 col-lg-4",
     button:           "btn btn-default",
