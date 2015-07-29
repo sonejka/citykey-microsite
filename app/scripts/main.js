6772ac926b071d5309f4c35114b374336c914dd5
Kseniya Varabei <kseniya.varabei@gmail.com>
2015-07-29 12:11:14 +0300

    update sidebar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,36 @@
 // jshint devel:true
-window.onload = function() {
+window.onload = function () {
 
   //$("#welcome-modal").modal();
 
-    $( ".sidebar-nav-wrap" ).hover(function() {
-            $(".content-with-sidebar").toggleClass("sidebar-opened");
-    });
 
-    $( ".shadow-box" ).hover(function() {
-        $(".content-with-sidebar").toggleClass("sidebar-opened");
-    });
+  $('.sidebar-nav-wrap, .shadow-box').mouseenter(function () {
+    $(this).addClass('mouse_over');
+    $(".content-with-sidebar").addClass("sidebar-opened");
+  })
+
+  var timeoutId;
+  $('.sidebar-nav-wrap, .shadow-box').mouseleave(function () {
+    var $this = $(this);
+    timeoutId = setTimeout(function () {
+      $this.removeClass('mouse_over');
+      if ($('.mouse_over').length == 0) {
+        $(".content-with-sidebar").removeClass("sidebar-opened");
+      }
+    }, 20);
+  });
 
 
   $('#instagram-feed').pongstgrm({
-    accessId:     '2121083481',
-    accessToken:  '2121083481.1677ed0.8737974840cf46309a44befee7cfdd38',
-    show:         'feed',
-    count:         6,
-    column:        "col-xs-12 col-sm-4 col-md-4 col-lg-4",
-    button:           "btn btn-default",
+    accessId: '2121083481',
+    accessToken: '2121083481.1677ed0.8737974840cf46309a44befee7cfdd38',
+    show: 'feed',
+    count: 6,
+    column: "col-xs-12 col-sm-4 col-md-4 col-lg-4",
+    button: "btn btn-default",
     comments: false,
-    effects:    "scaling"});
-
-
+    effects: "scaling"
+  });
 
 
 }
